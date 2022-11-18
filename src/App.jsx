ce7aec5eb1840aa6c9502c1f606bbef8440fb26a
Mauro Alfonso <maurogalfonso@gmail.com>
2022-11-18 12:47:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Client,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+import styles from "./style";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.flexCenter} ${styles.paddingX}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Client />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
